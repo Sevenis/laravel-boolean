0bddeb14d905f99f09459ea6b188e53d496552fe
Pietro <pietrosette@gmail.com>
2020-10-13 20:13:21 +0200

    Check route ID/SLUG da vedere

--- resources/views/studenti.blade.php
+++ resources/views/studenti.blade.php
@@ -8,7 +8,7 @@
           <div class="card">
             <div class="img-student">
                 <img src="{{$student['img']}}" alt="{{$student['nome']}}">
-                <a href="{{route('Student.slug', ['slug' => $key])}}"><h3 class="card-title">{{$student['nome']}} ({{$student['anni']}} anni)</h3></a>
+                <a href="{{route('Student.show', ['id' => $key])}}"><h3 class="card-title">{{$student['nome']}} ({{$student['anni']}} anni)</h3></a>
             </div>
             <div class="card-body">
               <h4 class="card-text"> Assunt{{(($student['genere'] == 'f') ? 'a' : 'o')}} da: {{$student['azienda']}}</h4>
